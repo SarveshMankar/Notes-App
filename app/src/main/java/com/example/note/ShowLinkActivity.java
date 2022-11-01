13bef74e96cd55e2b276f4c6b73eadee9d27965f
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-11-01 20:57:38 +0530

    Added Menu Options on Toolbar

--- app/src/main/java/com/example/note/ShowLinkActivity.java
+++ app/src/main/java/com/example/note/ShowLinkActivity.java
@@ -7,7 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -37,12 +40,39 @@ public class ShowLinkActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.showmenu, menu);
+
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
                 return true;
+            case R.id.next:
+                Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.previous:
+                Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
+                return true;
         }
+
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+
+    
+    public void nextLink(View view){
+        Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void previousLink(View view){
+        Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
     }
 }
